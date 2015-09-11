ae8a761135b06788b52096149df0d6f9c852a99a
doruu12 <dungureanu@gmail.com>
2015-09-11 17:13:47 +0300

    Update to Node v4.0.0

--- src/dotnet/edge.cpp
+++ src/dotnet/edge.cpp
@@ -3,20 +3,19 @@
 BOOL debugMode;
 BOOL enableScriptIgnoreAttribute;
 
-//NAN_METHOD(initializeClrFunc)
-//{
-//    return ClrFunc::Initialize(info);
-//}
+NAN_METHOD(initializeClrFunc)
+{
+    return ClrFunc::Initialize(info);
+}
 
 #pragma unmanaged
 void init(Handle<Object> target) 
 {
- //   debugMode = (0 < GetEnvironmentVariable("EDGE_DEBUG", NULL, 0));
-	//debugMode = true;//Doru
- //   DBG("edge::init");
- //   V8SynchronizationContext::Initialize();
- //   enableScriptIgnoreAttribute = (0 < GetEnvironmentVariable("EDGE_ENABLE_SCRIPTIGNOREATTRIBUTE", NULL, 0));
-    //Nan::SetMethod(target, "initializeClrFunc", initializeClrFunc);//Doru
+    debugMode = (0 < GetEnvironmentVariable("EDGE_DEBUG", NULL, 0));
+    DBG("edge::init");
+    V8SynchronizationContext::Initialize();
+    enableScriptIgnoreAttribute = (0 < GetEnvironmentVariable("EDGE_ENABLE_SCRIPTIGNOREATTRIBUTE", NULL, 0));
+    Nan::SetMethod(target, "initializeClrFunc", initializeClrFunc);
 }
 
 #pragma unmanaged
